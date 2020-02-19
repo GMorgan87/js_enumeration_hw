9709d4f72191ee4ce6312f2655c03e7bc6b59675
Gmorgan87 <gmorgan87@hotmail.co.uk>
2020-02-19 16:50:46 +0000

    adds extensions

--- models/traveller.js
+++ models/traveller.js
@@ -12,20 +12,30 @@ return this.journeys.map((journey) => journey.endLocation)
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+  return this.journeys.filter((journey) => journey.transport === transport)
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
+  return this.journeys.filter((journey) => journey.distance > minDistance)
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  return this.journeys.reduce((total, journey) => total + journey.distance, 0)
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
 
+  let onlyUnique = function (value, index, self) {
+    return self.indexOf(value) === index;
+  };
+
+  transportArray = this.journeys.map((journey) => journey.transport)
+  return transportArray.filter(onlyUnique)
 };
 
+// function onlyUnique(value, index, self) {
+//     return self.indexOf(value) === index;
+//     var a = ['a', 1, 'a', 2, '1'];
+//     var unique = a.filter( onlyUnique );
 
 module.exports = Traveller;
